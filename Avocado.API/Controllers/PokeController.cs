e093c77153ceb74852e066d95125d85d85adf7cb
Diego Martignoni <diego.martignoni@abenergie.it>
2021-06-08 01:39:14 +0200

    Updating api structure

---
+++ Avocado.API/Controllers/PokeController.cs
@@ -0,0 +1,20 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Avocado.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PokeController : ControllerBase
+    {
+        public PokeController()
+        {
+            
+        }
+        
+        [HttpGet]
+        public IActionResult Random()
+        {
+            return Ok("This is a random poke with avocado :)");
+        }
+    }
+}
--- Avocado.API/Startup.cs
+++ Avocado.API/Startup.cs
@@ -26,7 +26,7 @@ namespace Avocado.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
+            services.AddRouting(options => options.LowercaseUrls = true);
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
@@ -41,7 +41,11 @@ namespace Avocado.API
             {
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Avocado.API v1"));
+                app.UseSwaggerUI(c => 
+                {
+                    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Avocado.API v1");
+                    c.RoutePrefix = string.Empty;
+                });
             }
 
             app.UseHttpsRedirection();
