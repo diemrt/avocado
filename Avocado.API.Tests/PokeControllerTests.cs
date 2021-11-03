4b6f7a3acb9f731539277b1964110dbd0c495534
Diego Martignoni <diego.martignoni@abenergie.it>
2021-11-03 18:43:36 +0100

    Adding tests

---
+++ Avocado.API.Tests/PokeControllerTests.cs
@@ -0,0 +1,27 @@
+﻿using Avocado.API.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Avocado.API.Tests
+{
+    public class PokeControllerTests
+    {
+        private readonly PokeController _pokeController;
+
+        public PokeControllerTests()
+        {
+            _pokeController = new PokeController();
+        }
+
+        [Fact]
+        public void Random_MustFail()
+        {
+            var result = _pokeController.Random();
+            Assert.True(false);
+        }
+    }
+}
