0f817946958c5235b8ad73e3952be4a890795d81
Diego Martignoni <diego.martignoni@abenergie.it>
2021-11-03 18:11:39 +0100

    Fixing base repo

--- Avocado.API/Startup.cs
+++ Avocado.API/Startup.cs
@@ -40,13 +40,14 @@ namespace Avocado.API
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => 
-                {
-                    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Avocado.API v1");
-                    c.RoutePrefix = string.Empty;
-                });
             }
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c => 
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Avocado.API v1");
+                c.RoutePrefix = string.Empty;
+            });
 
             app.UseHttpsRedirection();
 
